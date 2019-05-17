7efe1066153fd90d60fbbc91f2ed359a0754750c
Zubair Rathore <z_rathore@hotmail.com>
2019-05-17 17:06:25 -0400

    Added Hello world Program

---
+++ src/jenkins/testJenkins.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+public class testJenkins {
+
+	public static void main(String[] args) {
+
+		for(int i=0; i < 10; i++) {
+			System.out.println("Hello Jenkins for the :" + i);
+		}
+	}
+
+}
